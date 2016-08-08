777d4e035adbc08afa6e4210425d6dfb5a534624
Albrecht Weiche <weich@posteo.de>
2016-08-08 10:39:29 +0200

    updated dpdk dependencis for VM

--- INSTALL_sfc/scripts/dep_dpdk_in_vm.sh
+++ INSTALL_sfc/scripts/dep_dpdk_in_vm.sh
@@ -6,4 +6,5 @@ mv dpdk-16.04 dpdk
 
 # sudo ./l2fwd_filter/build/l2fwd_filter -n 3 -b 0000:00:03.0 -- -p 3
 # sudo ./simple_bench/build/simple_bench -n 3 -b 0000:00:03.0 -- -p 3 -m 02:00:00:00:00:00 -s 10.1.0.3 -d 10.1.0.9
-# sudo ./packet_gen/build/packet_gen -n 1 -b 0000:00:03.0 -b 0000:00:05.0 -- -s 10.1.0.3 -d 10.1.0.8 -m 02:00:00:00:00:00
+# sudo ./packet_gen/build/packet_gen -n 1 -b 0000:00:03.0 -- -s 10.1.0.3 -d 10.1.0.8 -m 02:00:00:00:00:00
+# sudo ./logging/build/logging -n 1 -b 0000:00:03.0
